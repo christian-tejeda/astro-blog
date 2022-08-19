5ff58445847504576dcad86bbdf1cba563da6334
Christian Tejeda <christ.0@outlook.com>
2022-08-19 13:51:48 -0300

    Restyling part 1

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog personal';
+export const SITE_DESCRIPTION = '¡Bienvenido a mi sitio web!';
