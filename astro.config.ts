b199159fe75c21de371e7edcc392c9aa92485260
Christian Tejeda <christ.0@outlook.com>
2022-08-22 17:05:05 -0300

    Another go at deploying on Vercel

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/christian-tejeda/astro-blog',
+  site: '/',
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel()
