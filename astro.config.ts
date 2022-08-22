013106d15f0b156cb9170caec75a888c39337278
Christian Tejeda <christ.0@outlook.com>
2022-08-22 16:01:39 -0300

    Add Vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://github.com/christian-tejeda/astro-blog',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel()
 });
